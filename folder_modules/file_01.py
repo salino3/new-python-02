4c7f0d4db681bd10f730b3d6c915385181e1f131
Salino3 <flaviobcn012@gmail.com>
2025-12-30 17:25:33 +0200

    add import module

--- folder_modules/file_01.py
+++ folder_modules/file_01.py
@@ -1,2 +1,32 @@
-
+#!/usr/bin/env python3 
+import os
+ 
+ 
 print("Print from file_01.py!")
+print(os.path.dirname(os.path.abspath(__file__))) # root current folder
+print(__name__) 
+__counter = 0 # convention for saying do not modify it, but it possible
+ 
+ 
+def suml(the_list):
+  global __counter
+  __counter += 1
+  the_sum = 0
+  for element in the_list:
+   the_sum += element
+  return the_sum
+ 
+ 
+def prodl(the_list):
+  global __counter
+  __counter += 1
+  prod = 1
+  for element in the_list:
+   prod *= element
+  return prod
+ 
+ 
+ 
+my_list = [i+1 for i in range(5)]
+print(suml(my_list) == 15)
+print(prodl(my_list) == 120) 
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import folder_modules.file_01
+import folder_modules.file_01 as file_01
+import sys
+
+# add new route module
+sys.path.append(r"C:\Users\*****\*****\new-python-02")
+for p in sys.path:
+  print(p) 
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(file_01.suml(zeroes))
+print(file_01.prodl(ones)) 
+
+ 
