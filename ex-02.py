a5128d5c644b2549d5b40d1368ac9e8610d32b12
Salino3 <flaviobcn012@gmail.com>
2025-12-30 16:20:50 +0200

    save changes

--- ex-02.py
+++ ex-02.py
@@ -1,6 +1,9 @@
 import platform
 import random  
 import subprocess
+from platform import python_implementation, python_version_tuple
+import os
+ 
 
 my_list = [1, 2, 3, -4.8, 5.9, 6, 7, 8, 9, 10, 3, 400, 5, 6, 7, 8, 9, 10]
 
@@ -17,7 +20,9 @@ print(platform.platform())
 print(platform.platform(1))
 print(platform.platform(0, 1)) 
 print(platform.machine()) 
+print(platform.version()) 
 print(platform.processor())
+print(platform.system())
 
 
 cpu = platform.processor()
@@ -29,3 +34,13 @@ if "Intel" in cpu:
     print("Es Intel")
 
 
+print("------------------------")
+ 
+print(python_implementation())
+
+for atr in python_version_tuple():
+    print(atr)
+
+
+#
+print(dir(os) )
